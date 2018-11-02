c01ed7805fcd07bbe7be7aef032b83452b8a7f97
Benjamin Siegel <benjaminsiegel@Benjamins-MBP-2.home>
2018-11-01 21:55:26 -0400

    with authentication

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
 let submitButton = document.querySelector("#submitComment");
+let signInButton = document.querySelector("#signIn");
+let displayName, email;
 
 submitComment.addEventListener("click", e => {
   e.preventDefault();
-  let firstName = document.querySelector("#first_name").value;
-  let lastName = document.querySelector("#last_name").value;
-  let email = document.querySelector("#email").value;
+  //   let firstName = document.querySelector("#first_name").value;
+  //   let lastName = document.querySelector("#last_name").value;
+  //   let email = document.querySelector("#email").value;
   let comment = document.querySelector("#comment").value;
-  saveComment(firstName, lastName, email, comment);
+  saveComment(comment);
 });
 
-function saveComment(first, last, email, comment) {
+signInButton.addEventListener("click", () => {
+  signInUser();
+});
+
+function signInUser() {
+  console.log("User Signed In");
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(user);
+      displayName = user.displayName;
+      email = user.email;
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      console.log(errorMessage);
+    });
+}
+
+function saveComment(comment) {
+  document.querySelector("#userComments").innerHTML = "";
   let userComment = {
-    first: first,
-    last: last,
+    first: displayName,
+    last: displayName,
     email: email,
     comment: comment
   };
@@ -20,7 +57,10 @@ function saveComment(first, last, email, comment) {
   firebase
     .database()
     .ref("/comments")
-    .push(userComment);
+    .push(userComment)
+    .then(() => {
+      document.querySelector("#comment").value = "";
+    });
 }
 
 function getComments() {
